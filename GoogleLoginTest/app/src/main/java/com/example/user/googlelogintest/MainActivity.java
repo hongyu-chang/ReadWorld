3b533c5f0491a8b690a552b76ac52918bde5f2be
terry0921i <terry0921i@gmail.com>
2016-11-14 21:56:33 +0800

    16-11-14 finish begin_activity

--- GoogleLoginTest/app/src/main/java/com/example/user/googlelogintest/MainActivity.java
+++ GoogleLoginTest/app/src/main/java/com/example/user/googlelogintest/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.user.googlelogintest;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -17,25 +21,49 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.plus.Plus;
-import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+import static com.example.user.googlelogintest.R.id.flipper;
 
-    private SignInButton login;
-    private Button logout;
-    private TextView profile;
-    private ImageView profilePhoto;
+public class MainActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private TextView title;                     // app 標題
+    private TextView info;                      // app 副標題
+    private ImageView icon;                     // app icon
+    private ViewFlipper f;                      // 圖片自動播放
+    private SignInButton login;                 // google登入button
+    private Button guestButton;                 // 訪客登入
+
     private GoogleApiClient googleApiClient;
     private GoogleSignInOptions signInOptions;
     private static final int REQUEST_CODE = 100;
+
+    private int[] resid = {R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f, R.drawable.g, R.drawable.h, R.drawable.i, R.drawable.j};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title = (TextView) findViewById(R.id.appTitle);
+        info = (TextView) findViewById(R.id.appInfoText);
+        Typeface mainType = Typeface.createFromAsset(getAssets(),"fonts/HPSimplified_Bd.ttf");
+        Typeface subType = Typeface.createFromAsset(getAssets(),"fonts/HPSimplified.ttf");
+        title.setTypeface(mainType);
+        info.setTypeface(subType);
+
+        icon = (ImageView) findViewById(R.id.iconImage);
+        icon.setImageResource(R.drawable.icon);
+
+        f = (ViewFlipper) findViewById(flipper);
+        for(int i = 0; i < resid.length; i++) {
+            f.addView(getImageView(resid[i]));
+        }
+
+        f.setInAnimation(this, R.anim.left_in);
+        f.setOutAnimation(this, R.anim.left_out);
+        f.setFlipInterval(3000);
+        f.startFlipping();
 
         signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
@@ -47,12 +75,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 .addApi(Plus.API)
                 .build();
 
-        profilePhoto = (ImageView) findViewById(R.id.imageView);
-        profile = (TextView) findViewById(R.id.name);
-        login = (SignInButton) findViewById(R.id.login);
-        logout = (Button) findViewById(R.id.logout);
-        logout.setVisibility(View.INVISIBLE);
-
+        login = (SignInButton) findViewById(R.id.googleSignIn_button);
         login.setSize(SignInButton.SIZE_WIDE);
         login.setScopes(signInOptions.getScopeArray());
 
@@ -62,28 +85,17 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             public void onClick(View v) {
                 Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
                 startActivityForResult(signInIntent,REQUEST_CODE);
-
-                logout.setVisibility(View.VISIBLE);
             }
         });
         // [END logIn]
 
-        // [START logOut]
-        logout.setOnClickListener(new View.OnClickListener() {
+        guestButton = (Button) findViewById(R.id.guest);
+        guestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(
-                        new ResultCallback<Status>() {
-                            @Override
-                            public void onResult(@NonNull Status status) {
-                                profilePhoto.setImageResource(0);
-                                profile.setText("");
-                                logout.setVisibility(View.INVISIBLE);
-                            }
-                        });
+                Toast.makeText(v.getContext(), "這是一個Toast......", Toast.LENGTH_SHORT).show();
             }
         });
-        // [END logOut]
 
     } // [END onCreate]
 
@@ -100,22 +112,19 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             String id = account.getId(); // use id to identity
             Uri photoImage = account.getPhotoUrl();
 
-            if(photoImage != null) {
-                // user have profile picture
-                Picasso.with(this).load(photoImage).into(profilePhoto);
-            }
-            else {
-                // user don't have profile picture, use default picture
-                profilePhoto.setImageResource(R.drawable.profile_pic);
-            }
-
             // for google plus
             //Person person = Plus.PeopleApi.getCurrentPerson(googleApiClient);
-
-            profile.setText("Name: "+name+"\nEmail: "+email+"\n\nID: " +id);
-
         }
     } // [END onActivityResult]
+
+    // [START getImageView]
+    private ImageView getImageView(int resId){
+
+        ImageView image = new ImageView(this);
+        image.setBackgroundResource(resId);
+        return image;
+
+    } // [END getImageView]
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
